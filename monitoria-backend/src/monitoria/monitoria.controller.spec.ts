50e667a427d8b83027e88d5a5600be9891368eb0
Lucas Castro <lukhacastro120@gmail.com>
2025-07-08 14:59:29 -0300

    correções de teste ci
    - corrigindo PrismaModule em imports de MonitoriaService spec
    - corrigindo PrismaModule em imports de MonitoriaController spec

--- monitoria-backend/src/monitoria/monitoria.controller.spec.ts
+++ monitoria-backend/src/monitoria/monitoria.controller.spec.ts
@@ -1,12 +1,14 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { MonitoriaController } from './monitoria.controller';
 import { MonitoriaService } from './monitoria.service';
+import { PrismaModule } from '../prisma/prisma.module';
 
 describe('MonitoriaController', () => {
   let controller: MonitoriaController;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
+      imports: [PrismaModule],
       controllers: [MonitoriaController],
       providers: [MonitoriaService],
     }).compile();
--- monitoria-backend/src/monitoria/monitoria.service.spec.ts
+++ monitoria-backend/src/monitoria/monitoria.service.spec.ts
@@ -1,11 +1,13 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { MonitoriaService } from './monitoria.service';
+import { PrismaModule } from '../prisma/prisma.module';
 
 describe('MonitoriaService', () => {
   let service: MonitoriaService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
+      imports: [PrismaModule],
       providers: [MonitoriaService],
     }).compile();
 
